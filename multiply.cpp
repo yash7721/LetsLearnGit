d937335320cbb99eb3767ded394f9e2f71f3c0a8
Yash Ghatiya <yash.2002pramod@gmail.com>
2021-09-06 01:13:10 +0530

    added mulyiply file

--- multiply.cpp
+++ multiply.cpp
@@ -3,7 +3,6 @@ using namespace std;
 #define ll long long int
 int main(int argc, char const *argv[])
 {
-    cout<<"Hello World";
-    cout<<"Hello World2";
+    cout<<"hi this is multiply bramch";    
     return 0;
 }
