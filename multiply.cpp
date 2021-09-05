f0461f303863cc28a86574d1b3aa2f7344b33dd9
Yash Ghatiya <yash.2002pramod@gmail.com>
2021-09-06 01:40:43 +0530

    adding this for pushing

--- multiply.cpp
+++ multiply.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define ll long long int
 int main(int argc, char const *argv[])
 {
-    cout<<"hi this is multiply bramch";    
+    cout<<"hi this is multiply branch ";
+    cout<<"i am checking this function";    
     return 0;
 }
